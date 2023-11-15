02e6ab58898dcaad85da91cba5a9c58b0c920ee6
aromaltl <103577905+aromaltl@users.noreply.github.com>
2023-11-15 06:51:52 +0000

    mofified verify

--- verify.py
+++ verify.py
@@ -5,22 +5,27 @@ def run():
     try:
         for x in glob.glob('./*.csv'):
             df=pd.read_csv(x)
-            for i in df.columns:
+            for i in filter(lambda ff:True if 'RIGH' in ff or 'LEFT' in ff else False,df.columns):
+                
                 for j in df[i]:
                     t=eval(j)
                     for c in t:
-                        if len(c)!=5:
+                        if len(c)!=6:
                             print(x,c,'not correct')
                             return None
+                        if len(c[1])<3:
+                            print(x,c,'position invalid')
         print("!!!!!!!!!!!!!!!! all good !!!!!!!!!!!!!!!!!!!")
     except Exception as ex:
         
-
-        print('error occured',x,i)
         try:
-            print("#####",t,"########")
+            print('error occured',x,i)
+            print("########",t,"########")
+        except:
+            pass
         print(ex) 
 run()
+#!!!!!!!!!!!!!!!! all good !!!!!!!!!!!!!!!!!!!
 
 
 
